26e9530df9226a659a1b889dc2b2cdfaeb119b17
bsubburathinam <babu.subburathinam@accessdevelopment.com>
2016-08-10 17:12:50 -0600

    Registering a handler with a future.

--- src/main/scala/ScalaPongActor.scala
+++ src/main/scala/ScalaPongActor.scala
@@ -2,7 +2,9 @@ import akka.actor.{Actor, Status}
 
 class ScalaPongActor extends Actor {
   override def receive : Receive = {
-    case "Ping" => sender() ! "Pong"
+    case "Ping" => {
+      sender() ! "Pong" // executes in "akka dispatcher..." thread
+    }
     case _ => sender() ! Status.Failure(new Exception("unknown message"))
   }
 }
--- src/test/scala/ScalaPongActorTest.scala
+++ src/test/scala/ScalaPongActorTest.scala
@@ -1,3 +1,5 @@
+import java.io.{PrintWriter, StringWriter}
+
 import akka.actor.{ActorSystem, Props}
 import akka.util.Timeout
 import org.scalatest.{FunSpecLike, Matchers}
@@ -6,7 +8,7 @@ import akka.pattern.ask
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class ScalaPongActorTest extends FunSpecLike
+class ScalaPongActorTest extends FunSpecLike with Matchers
 {
   val system = ActorSystem()
 
@@ -14,11 +16,18 @@ class ScalaPongActorTest extends FunSpecLike
   val pongActor = system.actorOf(Props(classOf[ScalaPongActor]))
 
   describe("Pong actor") { // The subject of the test
+    import scala.concurrent.ExecutionContext.Implicits.global
     it ("should respond with Pong") { // Behavior expected from subject
       val future = pongActor ? "Ping"
-      val result = Await.result(future.mapTo[String], 1 second)
-      assert( "Pong" ===  result )
-    }
+      future
+        .onSuccess({
+          case "Pong" => { // executes in "Fork join pool" thread
+            println("Got pong.")
+          }
+          case _ => throw new IllegalStateException("Did not receive Pong.")
+        })
+      Thread.sleep(1000)
+   }
     it ("should fail on unknown message") {
       val future = pongActor ? "unknown"
       intercept[Exception] {
